9348b265ffd13ff91ec6c9bcc6c802922af7ef95
Luke Bearden <luke.bearden@gmail.com>
2016-10-26 23:56:58 -0400

    fixes storage check

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,17 @@
+// Feature detect + local reference
+var storage;
+var fail;
+var uid;
+try {
+	uid = new Date;
+	(storage = window.localStorage).setItem(uid, uid);
+	fail = storage.getItem(uid) != uid;
+	storage.removeItem(uid);
+	fail && (storage = false);
+} catch (exception) {}
+
+
+
 function start() {
 	new GameManager();
 }
